d6a952f8bda5f60829c6865f0a2eef9876762ee4
Ismael martinez <76608233+xIsm4@users.noreply.github.com>
2021-09-12 03:09:48 +0200

    Update LobbyCommandPlus.java

--- LobbyCommandPlus.java
+++ LobbyCommandPlus.java
@@ -10,71 +10,61 @@ import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 public class LobbyCommandPlus extends Plugin{
 
-	
-	public void onEnable() {
-		
-		
-		 getLogger().info(ChatColor.translateAlternateColorCodes('&', "&cLobbyCommand has been enabled"));
-		 ProxyServer.getInstance().getPluginManager().registerCommand(this, new Comando());
-		
-		
-		
-	}
-	
-	public void loadConfig() {
-		
-		try {
-			Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
-		} catch (IOException e) {
-		
-			e.printStackTrace();
-		}
-		
-		
-		
-		
-		
-	}
-	public void saveConfig() {
-		
-		try {
-			Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
-		} catch (IOException e) {
-		
-			e.printStackTrace();
-		}
-		
-		Configuration configuration = null;
-		try {
-			configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
-		} catch (IOException e1) {
-			
-			e1.printStackTrace();
-		}
-		
-		try {
-			ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, new File(getDataFolder(), "config.yml"));
-		} catch (IOException e) {
+    public void onEnable(){
+        
+        getLogger().info(ChatColor.translateAlternateColorCodes('&', "&cLobbyCommand has been enabled"));
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Comando());
+        
+    }
+    
+    public void loadConfig() {
 
-			e.printStackTrace();
-		}
-	}
-	
-	public void saveConfigBits() {
-		if (!getDataFolder().exists())
+        try {
+            Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+            
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+    }
+    public void saveConfig() {
+        try {
+            Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        Configuration configuration = null;
+        try {
+            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+        } catch (IOException e1) {
+
+            e1.printStackTrace();
+        }
+
+        try {
+            ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public void saveConfigBits() {
+        if (!getDataFolder().exists())
             getDataFolder().mkdir();
 
         File file = new File(getDataFolder(), "config.yml");
 
-   
-        if (!file.exists()) {
-            try (InputStream in = getResourceAsStream("config.yml")) {
+
+    if (!file.exists()) {
+         try (InputStream in = getResourceAsStream("config.yml")) {
                 Files.copy(in, file.toPath());
-            } catch (IOException e) {
-                e.printStackTrace();
+          } catch (IOException e) {
+              e.printStackTrace();
             }
         }
 
-		
-	}
+    }
 }
+
